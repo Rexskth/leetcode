a5e7b2ec47ca317b506409852a1d7cbbb9e97d53
Brajendra Singh Kaurav <121876377+Rexskth@users.noreply.github.com>
2025-02-13 22:25:27 +0530

    Time: 47 ms (72.12%) | Memory: 29 MB (85.11%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,40 +1,40 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end()); 
+        vector<vector<int>> ans; 
+        int n = nums.size();
 
-int  n=nums.size();
-sort(nums.begin(),nums.end());
+        for (int i = 0; i < n - 2; i++) {
+            // Skip duplicates for i
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
 
-for(int i=0;i<n;i++){
-   if (i != 0 && nums[i] == nums[i - 1]) continue;
+            int j = i + 1; // Left pointer
+            int k = n - 1; // Right pointer
 
-    int j=i+1;
-    int k=n-1;
+            while (j < k) {
+                int target = nums[i] + nums[j] + nums[k];
 
-    while(j<k){
-        int  sum = nums[i]+nums[j]+nums[k];
+                if (target == 0) { // Valid triplet found
+                    ans.push_back({nums[i], nums[j], nums[k]});
 
-        if(sum<0){
-            j++;
+                    // Skip duplicates for j and k
+                    while (j < k && nums[j] == nums[j + 1]) j++;
+                    while (j < k && nums[k] == nums[k - 1]) k--;
+
+                    // Move pointers
+                    j++;
+                    k--;
+                } else if (target < 0) {
+                    j++; // Increase the sum
+                } else {
+                    k--; // Decrease the sum
+                }
+            }
         }
-        else if(sum>0){
-            k--;
-        }
-        else{
-            vector<int> temp={nums[i],nums[j],nums[k]};
-            ans.push_back(temp);
-            j++;
-            k--;
-
-         while(j<k && nums[j]==nums[j-1]) j++;
-         while(j<k && nums[k]==nums[k+1]) k--;
-
-        }
-
-    }
-}
 
         return ans;
     }
 };
+
+
