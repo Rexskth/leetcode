a4d3d81e55d18a584b79b4f06d8692b5f26236ce
Brajendra Singh Kaurav <121876377+Rexskth@users.noreply.github.com>
2025-02-23 02:29:28 +0530

    Time: 29 ms (45.11%), Space: 16.2 MB (77.45%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,58 +1,43 @@
 class Solution {
-private:
-    void bfs(int startRow, int startCol, vector<vector<int>>& visited, vector<vector<char>>& grid) {
-        
-        visited[startRow][startCol] = 1;
-        queue<pair<int, int>> q;
-        q.push({startRow, startCol});
-        int n = grid.size();
-        int m = grid[0].size();
+    private: 
 
-        while (!q.empty()) {
-            int currentRow = q.front().first;
-            int currentCol = q.front().second;
-            q.pop();
+    void dfs(int i, int j, vector<vector<char>>& grid){
+       int row = grid.size();
+       int col = grid[0].size();
 
-            for (int delrow = -1; delrow <= 1; delrow++) {
-               
-                    int nrow = currentRow + delrow;
-                    
-                    if (nrow >= 0 && nrow < n && currentCol >= 0 && currentCol < m && grid[nrow][currentCol] == '1' && !visited[nrow][currentCol]) {
-                        visited[nrow][currentCol] = 1;
-                        q.push({nrow, currentCol});
-                    }
-                
-            }
-            
-                for (int delcol = -1; delcol <= 1; delcol++) {
-                   
-                    int ncol = currentCol + delcol;
-                    if (currentRow >= 0 && currentRow < n && ncol >= 0 && ncol < m && grid[currentRow][ncol] == '1' && !visited[currentRow][ncol]) {
-                        visited[currentRow][ncol] = 1;
-                        q.push({currentRow, ncol});
-                    }
-                }
-            
-        }
+       if(i < 0 || i >= row || j < 0 || j >= col || grid[i][j] == '0'){
+        return;
+       }
+
+       grid[i][j] = '0';
+
+       dfs(i, j + 1, grid);  // right
+       dfs(i + 1, j, grid);  // down
+       dfs(i - 1, j, grid);  // up
+       dfs(i, j - 1, grid); // left
+
     }
+
 
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int cnt = 0;
         int n = grid.size();
-        // if (n == 0) return 0;  // Handle the case where the grid is empty
         int m = grid[0].size();
-        vector<vector<int>> visited(n, vector<int>(m, 0));
 
-        for (int row = 0; row < n; row++) {
-            for (int col = 0;col< m; col++) {
-                if (!visited[row][col] && grid[row][col] == '1') {
+      if(n == 0) return 0;
+      int cnt = 0;
+
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(grid[i][j] == '1'){
                     cnt++;
-                    bfs(row, col, visited, grid);
+                    dfs(i, j, grid);
                 }
             }
-        }
 
+        }
         return cnt;
+        
     }
 };
