d978a607881acccd20fc0151d8352124e8291083
Brajendra Singh Kaurav <121876377+Rexskth@users.noreply.github.com>
2025-03-06 16:49:24 +0530

    Time: 306 ms (8.53%) | Memory: 11.8 MB (8.25%) - LeetSync

--- 1331-path-with-maximum-gold/path-with-maximum-gold.cpp
+++ 1331-path-with-maximum-gold/path-with-maximum-gold.cpp
@@ -1,20 +1,22 @@
 class Solution {
     private: 
-    int dfs(int i, int j, vector<vector<int>>& grid){
+    int dfs(int i, int j, vector<vector<int>>& grid, vector<vector<bool>>& visited){
         int n = grid.size();
         int m = grid[0].size();
 
-        if(i<0 || i>=n || j<0 || j>=m || grid[i][j] ==0){
+        if(i<0 || i>=n || j<0 || j>=m || grid[i][j] ==0 || visited[i][j]==true){
             return 0;
         }
 
         int gold = grid[i][j];
-        grid[i][j] = 0;
+        visited[i][j] = true;
+        // grid[i][j] = 0;
 
-        int maxgold = max({dfs(i,j-1,grid), dfs(i-1,j,grid),
-                            dfs(i+1,j,grid), dfs(i,j+1,grid)});
+        int maxgold = max({dfs(i,j-1,grid, visited), dfs(i-1,j,grid, visited),
+                            dfs(i+1,j,grid, visited), dfs(i,j+1,grid, visited)});
 
-            grid[i][j] = gold;
+            // grid[i][j] = gold;
+            visited[i][j] = false;
             return gold + maxgold;
     }
 public:
@@ -24,10 +26,12 @@ public:
         int n = grid.size();
         int m = grid[0].size();
 
+        vector<vector<bool>> visited(n, vector<bool>(m, false));
+
         for(int i = 0; i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]>0){
-                    maxG = max(maxG, dfs(i,j,grid));
+                    maxG = max(maxG, dfs(i,j,grid,visited));
                 }
             }
         }
