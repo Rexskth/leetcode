88395efcd04eb6bc3fe00eb0c201d6942bd9abec
Brajendra Singh Kaurav <121876377+Rexskth@users.noreply.github.com>
2025-03-07 22:17:24 +0530

    Time: 183 ms (57.59%), Space: 23.3 MB (36.85%) - LeetHub

--- 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.cpp
+++ 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.cpp
@@ -1,40 +1,34 @@
 class Solution {
-private:
-    bool isPrime(int n) {
-        if (n <= 1) return false;
-        if (n == 2) return true;
-        if (n % 2 == 0) return false;
-        for (int i = 3; i * i <= n; i += 2) {
-            if (n % i == 0) return false;
-        }
-        return true;
-    }
-
 public:
     vector<int> closestPrimes(int left, int right) {
-        vector<int> primes;
-        
-        for (int i = left; i <= right; i++) {
-            if (isPrime(i)) {
-                primes.push_back(i);
+        vector<bool> primes(right+1, true);
+        primes[0]=false; primes[1]=false;
+        for(int i=2; i*i<=right; i++){
+            if(!primes[i])
+                continue;
+            for(int j=i*i; j<=right; j+=i){
+                primes[j]=false;
             }
         }
 
-        if (primes.size() < 2) {
+        vector<int> primeNumbers;
+        for (int i = left; i <= right; i++) {
+            if (primes[i]) {
+                primeNumbers.push_back(i);
+            }
+        }
+        if (primeNumbers.size() < 2) {
             return {-1, -1};
         }
-
         int minDiff = INT_MAX;
-        vector<int> result = {-1, -1};
-
-        for (int i = 1; i < primes.size(); i++) {
-            int diff = primes[i] - primes[i - 1];
+        vector<int> answer;
+        for (int i = 0; i < primeNumbers.size() - 1; i++) {
+            int diff = primeNumbers[i + 1] - primeNumbers[i];
             if (diff < minDiff) {
                 minDiff = diff;
-                result = {primes[i - 1], primes[i]};
+                answer = {primeNumbers[i], primeNumbers[i + 1]};
             }
         }
-
-        return result;
+        return answer;
     }
 };
