fa2956e1a5637b014097a9a477838f14b422a355
Brajendra Singh Kaurav <121876377+Rexskth@users.noreply.github.com>
2025-03-16 22:39:50 +0530

    Time: 0 ms (100%), Space: 8.3 MB (9.89%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,19 +1,20 @@
 class Solution {
     private:
 
-    int f(int n, vector<int>& dp){
-        if(n < 2){
-            return dp[n] = n;
-        }
-        if(dp[n] != -1) return dp[n];
-
-        return dp[n] = f(n-1, dp) + f(n-2, dp);
-    }
+   
 public:
     int fib(int n) {
-        vector<int> dp(n+1, -1);
+        vector<int> dp(n+2);
 
-   return f(n, dp);
+       dp[0] = 0;
+
+       dp[1] = 1;
+
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+      
+      return dp[n];
         
     }
 };
