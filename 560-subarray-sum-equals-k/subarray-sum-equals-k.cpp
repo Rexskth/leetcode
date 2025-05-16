31d0a8458ef011b86f730a28f1c23d7ba9f65560
Brajendra Singh Kaurav <121876377+Rexskth@users.noreply.github.com>
2025-05-17 00:17:40 +0530

    Time: 34 ms (91.40%) | Memory: 45.2 MB (77.15%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,29 +1,24 @@
-#include <unordered_map>
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int, int> prefixSumCount;
-        int sum = 0;
-        int ans = 0;
 
-        // Initialize with prefix sum zero to handle cases where sum directly equals `k`
-        prefixSumCount[0] = 1;
+        unordered_map<int, int> Map;
+        Map[0]=1;
 
-        for (int num : nums) {
-            sum += num;
+        int presum = 0;
+        int cnt = 0;
 
-            // Check if there exists a prefix sum that, when removed, gives sum `k`
-            if (prefixSumCount.find(sum - k) != prefixSumCount.end()) {
-                ans += prefixSumCount[sum - k];
+
+        for(int num : nums){
+            presum += num;
+
+            if(Map.find(presum-k) != Map.end()){
+                cnt += Map[presum-k];
             }
 
-            // Update the count of the current prefix sum in the map
-            prefixSumCount[sum]++;
-        }
+            Map[presum]++;
 
-        return ans;
+        }
+        return cnt;
     }
 };
