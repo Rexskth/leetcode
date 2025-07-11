427edbaecd92dd80c419b1239dc2bdc20971bd15
Brajendra Singh Kaurav <121876377+Rexskth@users.noreply.github.com>
2025-07-11 08:25:34 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (87.11%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -11,28 +11,40 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        
+
           ListNode *dummy, *temp;
         dummy = new ListNode();
         temp = dummy;
-        
-        //when both list1 and list2 isn't empty
-        while(list1 && list2){
-            if(list1->val < list2->val){
-                temp->next = list1;
-                list1 = list1->next;
-            }
-            else{
-                temp->next = list2;
-                list2 = list2->next;   
-            }
-            temp = temp->next;
+
+        while(list1 != NULL && list2 != NULL){
+
+        if(list1->val < list2->val){
+            dummy->next = list1;
+            dummy = dummy->next;
+            list1 = list1->next;
+        }else{
+            dummy->next = list2;
+            dummy = dummy->next;
+            list2 = list2->next;
         }
+        }
+
+        while(list2 != NULL){
+            dummy->next =list2;
+            dummy = dummy->next;
+            list2 = list2->next;
+        }
+
+        while(list1 != NULL){
+            dummy->next = list1;
+            dummy = dummy->next;
+            list1 = list1->next;
+        }
+
+
         
-        // we reached at the end of one of the list
-        if(list1) temp->next = list1;
-        if(list2) temp->next = list2;
+
+        return temp->next;
         
-        return dummy->next;
     }
 };
