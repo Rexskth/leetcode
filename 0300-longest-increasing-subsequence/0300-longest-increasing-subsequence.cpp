cf250f43269537f47e64a05a76bd9b1aae425797
Brajendra Singh Kaurav <121876377+Rexskth@users.noreply.github.com>
2025-07-21 18:15:07 +0530

    Time: 0 ms (100%), Space: 13.9 MB (94.66%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,29 +1,13 @@
 class Solution {
 public:
+    int lengthOfLIS(vector<int>& A) {
+        int len = 0;
+        for(auto cur : A) 
+            if(len == 0 || A[len-1] < cur) A[len++] = cur;             // extend
+            else *lower_bound(begin(A), begin(A) + len, cur) = cur;    // replace
 
-int f(int indx, int prev, vector<int>& nums,  vector<vector<int>>& dp){
-      
-      if(indx<0){
-        return 0;
-      }
 
-
-        if (dp[indx][prev + 1] != -1) {
-            return dp[indx][prev + 1];  // Use memoized result
-        }
-
-      int npick = 0 + f(indx-1, prev, nums,dp);
-      int pick = 0;
-      if(prev == -1 || nums[indx] < nums[prev]){
-          pick = 1 + f(indx-1, indx, nums, dp);
-      }
-    
-    return dp[indx][prev + 1] = max(pick, npick);
-}
-    int lengthOfLIS(vector<int>& nums) {
-        int n =  nums.size();
-        int prev = -1;
-        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
-        return f(n-1,prev,nums,dp);
+            
+        return len;
     }
 };
