ddb4201356f0f91f05302fb8251d7020f9e5af1e
Brajendra Singh Kaurav <121876377+Rexskth@users.noreply.github.com>
2025-07-21 18:10:40 +0530

    Time: 0 ms (100%), Space: 17.7 MB (56.48%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -4,14 +4,17 @@ public:
         int preffix = 1, suffix = 1;
         int ans = INT_MIN;
 
+
         for(int i = 0; i<nums.size();i++){
             if(preffix == 0) preffix = 1;
             if(suffix == 0) suffix = 1;
+
 
             preffix *= nums[i];
             suffix *= nums[nums.size()-i-1];
 
             ans = max(ans, max(preffix, suffix));
+            
         }
         
 
