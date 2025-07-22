a565bb39313634cf60d17c6ac9765c1aaab5c47f
Brajendra Singh Kaurav <121876377+Rexskth@users.noreply.github.com>
2025-07-22 09:23:46 +0530

    Time: 2071 ms (5.04%) | Memory: 7.9 MB (78.41%) - LeetSync

---
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numTrees(int n) {
+        if (n <= 1) return 1;
+
+        int total = 0;
+        for (int i = 1; i <= n; i++) {
+            // Recursive formula: left * right
+            total += numTrees(i - 1) * numTrees(n - i);
+        }
+        return total;
+    }
+};
